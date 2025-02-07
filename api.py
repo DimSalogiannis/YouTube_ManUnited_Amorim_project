173956378b774948305f29b88bb4f5a060c6657e
DimSalogiannis <mitsoss.salo@gmail.com>
2025-02-07 16:50:37 +0100

    adding the code for the video data

--- api.py
+++ api.py
@@ -18,3 +18,35 @@ MAX_RESULTS = 50 # maximum number of results to return
 DAILY_LIMIT = 10000 # maximum number of requests per day
 API_COST_PER_CALL = 100
 
+# the following code snippet is used to read the date that the previous request stopped at and continue from there,  
+# ensuring that no data is lost or duplicated in the process
+try:
+    with open('last_date.txt', 'r') as f:
+        last_date = f.read().strip()
+        if last_date:
+            START_DATE = last_date
+except FileNotFoundError:
+    pass
+
+def get_video_data(start_date, end_date):
+    all_videos_ids = []
+    next_page_token = None
+    api_calls = 0
+    
+    while True:
+        if api_calls >= DAILY_LIMIT / API_COST_PER_CALL:
+            break
+        
+    request = youtube.search().list(
+        part = 'snippet',
+        q = QUERY, 
+        type = 'video',
+        maxResults = MAX_RESULTS,
+        publishedAfter = start_date,
+        publishedBefore = end_date,
+        pageToke = next_page_token
+    )
+    response = request.execure()
+    api_calls += 1
+    
+    
